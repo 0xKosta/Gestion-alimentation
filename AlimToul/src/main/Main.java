f761e70899915d5d4e2ec07f4051d1d896ea4cb0
K <konstantin.andjelkovic03@gmail.com>
2022-12-01 22:11:56 +0100

    Project start - Added main function and weight analysis

---
+++ AlimToul/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import analyse.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Poids poids = new Poids(73.0f, 73.2f, 83.0f);
+		poids.poidsChangement();
+	}
+
+}
