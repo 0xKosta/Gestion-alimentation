c21a81b6b79306631887a8ad794c108f88967429
K <konstantin.andjelkovic03@gmail.com>
2022-12-15 19:20:38 +0100

    Work on DietOPtionsMenu

--- AlimToul/src/main/DietOptionsMenu.java
+++ AlimToul/src/main/DietOptionsMenu.java
@@ -18,7 +18,14 @@ public class DietOptionsMenu extends Menu{
 		System.out.println("What would you like to do?");
 		System.out.println("[1] Nutritional value lookup");
 		System.out.println("[2] Current diet");
-		System.out.println("[3] ");
+		System.out.println("[3] Suggest meal");
+		System.out.println("[4] Change diet");
+		int response = Integer.parseInt(inputOutput(null));
+		if (response == 1) {
+			System.out.println("TODO");
+		} else if (response == 2) {
+			System.out.println("TODO");
+		}
 	}
 
 }
